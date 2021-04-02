540120a9b72133664f2dc8fb869967678123ba99
Hansell Baran <hansellb@kth.se>
2021-04-02 16:57:47 +0200

    feat: add clarifai API, refactor, swagger
    - Added AWS Rekognition API & Clarifai API image analysis to the `/compare` endpoint
    - Added Swagger and `/api-docs`
    - Removed commented out code
    - Removed mongodb related code in UI scripts
    - Update pm2 version
    - Update package.json info
    - Updated web site to show Clarifai column and tags
    - Update .env zip file with new credentials
    - Display tags in UI for all 4 ML/AI APIs
    - Heavy refactoring (code style, new functions, jsdocs comments)
    - Added CORS, Crypto, Morgan and JS-YAML

--- public/script.js
+++ public/script.js
@@ -1,100 +1,9 @@
-function addProduct(event, element) {
-  event.preventDefault();
-
-  let data = {};
-
-  let formInputs = element.elements;
-
-  for (var i=0; i < formInputs.length; i++) {
-    let formInput = formInputs[i];
-    if (formInput.nodeName === 'INPUT' && formInput.type === 'text') {
-      if (formInput.name !== 'id') {
-        data[formInput.name] = formInput.value;
-      }
-    }
-  }
-
-  const config = {};
-
-  axios.post('/products', data, config)
-    .then(res => {
-      console.log(res.data);
-    })
-    .catch(err => {
-      handleAxiosErrors(err);
-    });
-}
-
-function getProducts() {
-  axios.get('/products')
-    .then(res => {
-      const products = res.data;
-      const productListElem = document.getElementById('existing_products');
-
-      let listHTML = '';
-
-      products.forEach(product => {
-        const listTagOpen = '<li' + ' id="' + product._id + '">';
-        const listTagClose = '</li>';
-        listHTML += listTagOpen + ' <span>' + product.name + '</span> <span>' + product.price + listTagClose + '</span>';
-      });
-      productListElem.innerHTML = listHTML;
-
-      products.forEach(product => {
-        let listElem = document.getElementById(product._id);
-        listElem.addEventListener('click', (event) => {
-          const id = event.currentTarget.id;
-          const name = event.currentTarget.querySelector('span:first-child').innerText;
-          const price = event.currentTarget.querySelector('span:nth-child(2)').innerText;
-
-          const formElem = document.getElementById('product_form');
-          const formInputs = formElem.elements;
-          for (var i=0; i < formInputs.length; i++) {
-            let formInput = formInputs[i];
-            switch (formInput.name) {
-              case 'id':
-                formInput.value = id;
-                break;
-              case 'name':
-                formInput.value = name;
-                break;
-              case 'price':
-                formInput.value = price;
-                break;
-              default:
-                break;
-            }
-          }
-        });
-      });
-    })
-    .catch(err => {
-      handleAxiosErrors(err);
-    });
-}
-
-function updateProduct() {
-  const formElem = document.getElementById('product_form');
-  const formInputs = formElem.elements;
-
-  let data = {};
-  for (var i=0; i < formInputs.length; i++) {
-    let formInput = formInputs[i];
-    if (formInput.nodeName === 'INPUT') {
-        data[formInput.name] = formInput.value;
-    }
-  }
-
-  axios.put('/products', data)
-  .then(res => {
-    console.log(res);
-    getProducts();
-  })
-  .catch(err => {
-    handleAxiosErrors(err);
-  });
-}
-
+/**
+ * Loads images
+ *
+ * @param {*} event
+ * @param {*} element
+ */
 function loadImages(event, element) {
   const imgContainer = document.querySelector('.flex-container');
 
@@ -117,12 +26,17 @@ function loadImages(event, element) {
   });
 }
 
+/**
+ * Upload an image
+ *
+ * @param {*} event
+ * @param {*} element
+ */
 function uploadImage(event, element) {
   event.preventDefault();
 
-  const formData = new FormData(element);
-
-  const config = {};
+  const formData  = new FormData(element);
+  const config    = {};
 
   axios.post('/img', formData, config)
   .then(res => {
@@ -134,6 +48,12 @@ function uploadImage(event, element) {
   });
 }
 
+/**
+ * Delete an image
+ *
+ * @param {*} imgName
+ * @param {*} element
+ */
 function deleteImage(imgName, element) {
   axios.delete('/img/' + imgName)
   .then(res => {
@@ -146,38 +66,78 @@ function deleteImage(imgName, element) {
   });
 }
 
+/**
+ * Compare Image Analysis results from different APIs
+ *
+ * @param {*} event
+ * @param {*} element
+ */
 function getImgAnalysis(event, element) {
   event.preventDefault();
 
-  const formData = new FormData(element);
-
-  const config = {};
+  const formData  = new FormData(element);
+  const config    = {};
 
   axios.post('/compare', formData, config)
   .then(res => {
-    console.log(res.data);
-    const azureUl = document.getElementById('azure_tags');
-    const googleUl = document.getElementById('google_labels');
-    const awsUl = document.getElementById('aws_labels');
+    // console.log(res.data);
+    const azureUl     = document.getElementById('azure_tags');
+    const googleUl    = document.getElementById('google_labels');
+    const awsUl       = document.getElementById('aws_labels');
+    const clarifaiUl  = document.getElementById('clarifai_concepts');
 
+    let awsLabels = '';
     let azureTags = '';
-    res.data.azure.forEach(tag => {
-      azureTags += '<li>' + tag.name + ' -> ' + tag.confidence + '</li>';
-    });
-
+    let clarifaiConcepts = '';
     let googleLabels = '';
-    res.data.google.forEach(label => {
-      googleLabels += '<li>' + label.description + ' -> ' + label.topicality + '</li>';
+
+    Object.keys(res.data).forEach(api => {
+      switch(api) {
+        case 'aws':
+          res.data.aws.forEach(label => {
+            awsLabels += '<li>' + label.Name + ' -> ' + label.Confidence + '</li>';
+          });
+          break;
+        case 'azure':
+          res.data.azure.forEach(tag => {
+            azureTags += '<li>' + tag.name + ' -> ' + tag.confidence + '</li>';
+          });
+          break;
+        case 'clarifai':
+          res.data.clarifai.forEach(concept => {
+            clarifaiConcepts += '<li>' + concept.name + ' -> ' + concept.value + '</li>';
+          });
+          break;
+        case 'google':
+          res.data.google.forEach(label => {
+            googleLabels += '<li>' + label.description + ' -> ' + label.topicality + '</li>';
+          });
+          break;
+        default:
+          console.log(api);
+          break;
+      }
     });
 
-    azureUl.innerHTML = azureTags;
-    googleUl.innerHTML = googleLabels;
+    // res.data[api].forEach(label => {
+
+    // });
+
+    awsUl.innerHTML       = awsLabels;
+    azureUl.innerHTML     = azureTags;
+    googleUl.innerHTML    = googleLabels;
+    clarifaiUl.innerHTML  = clarifaiConcepts;
   })
   .catch(err => {
     handleAxiosErrors(err);
   });
 }
 
+/**
+ * Axios error handler
+ *
+ * @param {*} err
+ */
 function handleAxiosErrors(err) {
   // https://github.com/axios/axios#handling-errors
   if (err.response) {
